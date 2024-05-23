8fcb7b9a846ea9df655d8d3f92fa48abbc0eb6e5
flashcode <laughingliangcl@gmail.com>
2024-05-23 12:27:36 +0800

    feat: load css style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
+
 import { ConfigProvider } from "antd";
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,19 +19,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ConfigProvider
-          theme={{
-            token: {
-              // Seed Token，影响范围大
-              colorPrimary: "#0c0",
-              borderRadius: 4,
-              // 派生变量，影响范围小
-              colorBgContainer: "#f6ffed",
-            },
-          }}
-        >
-          {children}
-        </ConfigProvider>
+        <AntdRegistry>
+          <ConfigProvider
+            theme={{
+              token: {
+                // Seed Token，影响范围大
+                colorPrimary: "#0c0",
+                borderRadius: 4,
+                // 派生变量，影响范围小
+                colorBgContainer: "#f6ffed",
+              },
+            }}
+          >
+            {children}
+          </ConfigProvider>
+        </AntdRegistry>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,6 +15,8 @@ export default function Home() {
   const [contract, setContract] = useState(false);
   const searchData = async () => {
     setLoading(true);
+    setTx(null);
+    setShowInfo(false);
     setText("");
     console.log("input: ", input);
     const { data } = await fetch(`/api/detect?input=${input}`, {
@@ -73,8 +75,6 @@ export default function Home() {
         value={input}
         onChange={(e) => {
           setInput(e.target.value);
-          setTx(null);
-          setShowInfo(false);
         }}
         onSearch={searchData}
         size="large"
@@ -82,15 +82,15 @@ export default function Home() {
       />
       {tx && (
         <div className="mt-2 w-full">
-          <h3 className="text-lg mb-2">{input} is a hash</h3>
+          <h3 className="text-lg mb-2 break-words">{input} is a hash</h3>
           <div className="flex flex-col lg:flex-row">
             <div className="flex-1">
-              <MemoizedReactMarkdown className="break-all">
+              <MemoizedReactMarkdown className="break-words">
                 {text}
               </MemoizedReactMarkdown>
             </div>
             <div className="break-all flex-1">
-              <TextArea rows={30} value={tx}></TextArea>
+              <TextArea rows={35} value={tx}></TextArea>
             </div>
           </div>
         </div>
